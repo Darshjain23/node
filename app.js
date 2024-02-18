6859970b0991833fddad878c49b34687979b12a2
Darsh <darshjain2310@gmail.com>
2024-02-18 15:32:54 +0530

    18 jan

--- app.js
+++ app.js
@@ -4,18 +4,18 @@
 import express from 'express'
 const app = express()
 app.get('/',function(req,res){
-    res.write("<h1>Hello world!</h1>")
-    res.send()
+    // res.write("<h1>Hello world!</h1>")
+    res.sendFile('C:\\Users\\Admin\\Downloads\\project1\\views\\index.html')
 })
 
 app.get('/product',function(req,res){
-    res.write("<h1>your products are here</h1>")
-    res.send()
+    // res.write("<h1>your products are here</h1>")
+    res.sendFile('C:\\Users\\Admin\\Downloads\\project1\\views\\product.html')
 })
 
 app.get('/contact',function(req,res){
-    res.write("<h1>contact here!</h1>")
-    res.send()
+    // res.write("<h1>contact here!</h1>")
+    res.sendFile('C:\\Users\\Admin\\Downloads\\project1\\views\\contact.html')
 })
 
 
