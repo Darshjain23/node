948eb413fb2f52b75aeacb35dbe4e30b906c9dc4
Darsh <darshjain2310@gmail.com>
2024-02-27 20:20:12 +0530

    27 feb

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import express from 'express'
+import nodemailer from 'nodemailer'
+import 'dotenv/config'
 
 
 const __filename = fileURLToPath(import.meta.url);
@@ -8,6 +10,23 @@ const __dirname = dirname(__filename);
 const app = express()
 
 app.use(express.static(__dirname+'\\public'))
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.email,
+      pass: process.env.password
+    }
+  });
+  
+  var mailOptions = {
+    from: 'darsh10@somaiya.edu',
+    to: 'madhurima.v@somaiya.edu',
+    subject: 'Sending Email using Node.js',
+    text: 'Thank you for contacting us',
+    html: '<h1>Welcome</h1><p>Please dont text us again</p>'
+  };
+  
 
 app.get('/',function(req,res){
     // res.write("<h1>Hello world!</h1>")
@@ -22,6 +41,18 @@ app.get('/product',function(req,res){
 app.get('/contact',function(req,res){
     // res.write("<h1>contact here!</h1>")
     res.sendFile(__dirname+'\\views\\contact.html')
+})
+
+app.post('/contact',function(req,res){
+    console.log(process.env.email)
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+    // console.log("hello beautiful")
 })
 
 
